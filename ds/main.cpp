16503cfee5faa81cf0caf7c9abd844eb3d899433
leukelele <70491331+leukelele@users.noreply.github.com>
2025-10-16 13:21:38 -0500

    add working config parser to main

--- ds/main.cpp
+++ ds/main.cpp
@@ -1,3 +1,17 @@
+#include "config.hpp"
+#include <iostream>
+#include <string>
+
 int main() {
+
+    Config cfg;
+    const std::string path = CONFIG_FILE_PATH;
+
+    if (!parse_config(path, cfg)) {
+        std::cerr << "Failed to parse config file: " << path << "\n";
+        return 1;
+    }
+    print_config(cfg);
+
     return 0;
 }
