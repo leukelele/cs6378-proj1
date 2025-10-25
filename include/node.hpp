9b8a5a36a11f74708718c3f5d6c8c4d16f2e6f34
leukelele <70491331+leukelele@users.noreply.github.com>
2025-10-24 22:17:01 -0500

    documentation

--- include/node.hpp
+++ include/node.hpp
@@ -3,6 +3,20 @@
 
 #include <string>
 
+/**
+ * @brief represents the identifying information for a single node.
+ *
+ * Each NodeInfo structure stores the minimal information required to
+ * establish a network connection to a node in the distributed system:
+ *   - a unique node identifier (ID),
+ *   - the hostname or IP address where the node is running,
+ *   - and the port number used for communication.
+ *
+ * @param id   unique integer identifier assigned to the node.
+ * @param host hostname or IP address of the node.
+ * @param port TCP port number on which the node listens for incoming
+ *        messages.
+ */
 struct NodeInfo {
     int id;
     std::string host;
